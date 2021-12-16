bffc6e9469204e9c003720aa28c87026b904a77a
Zachary Kirkeby <95650247+ZacharyKirkeby@users.noreply.github.com>
2021-12-16 15:15:15 -0600

    Day5 part 2

--- Main.java
+++ Main.java
@@ -41,7 +41,6 @@ for(int i = 0; i < points.size(); i++){
     for(int k = minY; k <= maxY; k++){
       int x = points.get(i).getX1();
       int y = k;
-     // System.out.println(x + ", " + y);
       map[x][y] += 1;
       
     }
@@ -49,13 +48,53 @@ for(int i = 0; i < points.size(); i++){
     for(int m = minX; m <= maX; m++){
       int x = m;
       int y = points.get(i).getY1();
-      //System.out.println(x + ", " + y);
       map[x][y] += 1;
     }
+}else {
+	int y1 = points.get(i).getY1();
+	int y2 = points.get(i).getY2();
+	int x1 = points.get(i).getX1();
+	int x2 = points.get(i).getX2();
+			
+			
+	int slope = (y2 - y1) / (x2 - x1);
+	if (slope == 1) {
+		if (x1 < x2) {
+			int y = y1;
+			for (int x = x1; x <= x2; x++) {
+				map[x][y]++;
+				y++;
+			}
+		} else if (x2 < x1) {
+			
+			int y = y2;
+			for (int x = x2; x <= x1; x++) {
+				map[x][y]++;
+				y++;
+			}
+		}
+	} else if (slope == -1) {
+		if (x1 < x2) {
+			int y = y1;
+			for (int x = x1; x <= x2; x++) {
+				map[x][y]++;
+				y--;
+			}
+		} else if (x2 < x1) {
+			int y = y2;
+			for (int x = x2; x <= x1; x++) {
+				map[x][y]++;
+				y--;
+			}
+		}
+			
 }
+	
+
+}	
+	
 }
 
-   //testing to see why the count isnt working, need count backward and forward not just forward
 
 for(int l = 0; l < map.length; l++){
   for(int coll = 0; coll < map[0].length; coll++){
