6034def28e850b1d69afae8e9f2e3a44847f968a
Zachary Kirkeby <95650247+ZacharyKirkeby@users.noreply.github.com>
2022-01-01 12:29:26 -0600

    Day 11 Part 2

---
+++ Day11Part2.java
@@ -0,0 +1,95 @@
+package days;
+import java.io.File;
+import java.util.Scanner;
+public class Day11Part2 {
+	private static int flash;
+	
+	public static void main(String[] args) {	
+		Scanner reader = null;
+		int[][] grid = new int[10][10];
+		try {
+			reader = new Scanner(new File("inputDay11.txt"));
+		} catch (Exception e) {
+			System.out.println("file not found");
+		}
+		
+		int row = 0;
+		while (reader.hasNext()) {
+			String line = reader.nextLine();
+			for (int col = 0; col < line.length(); col++) {
+				grid[row][col] = Integer.parseInt(line.substring(col, col + 1));
+			}
+			row++;
+		}
+		reader.close();
+
+		int days = 0;
+		int[][] grid2 = grid;
+		while(allZeros(grid2) == false) {
+			updateGrid(grid2);
+			days++;
+		}
+		System.out.println(days);
+		
+		
+	}
+	
+	public static void updateGrid(int[][] grid) {
+		for (int r = 0; r < grid.length; r++) {
+			for (int c = 0; c < grid[0].length; c++) {
+				grid[r][c]++;
+			}
+		}
+		tens(grid);
+	}
+		
+		
+	public static void tens (int[][] grid) {
+		
+		boolean flasH = false;
+		for (int r = 0; r < grid.length; r++) {
+			for (int c = 0; c < grid[0].length; c++) {
+				if (grid[r][c] >= 10) {
+					grid[r][c] = 0;
+					flash++;
+					flasH = true;
+					allFlash(grid, r, c);
+					}
+				}
+			}
+			if (flasH == true) {
+				tens(grid);
+			}
+		
+		}
+		
+	
+	public static void allFlash(int[][] grid, int r, int c) {
+		int[] d = new int[] { -1, 0, 1 };
+		for (int i = 0; i < d.length; i++) {
+			for (int j = 0; j < d.length; j++) {
+				if (!(i == 1 && j == 1)) {
+					if (canFlash(grid, r + d[i], c + d[j]) == true) {
+						grid[r + d[i]][c + d[j]]++;
+					}	
+				}
+			}
+		}
+	}
+
+
+	public static boolean canFlash(int[][] grid, int r, int c) {
+		return (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] != 0);
+	}
+
+	public static boolean allZeros(int[][] grid) {
+		for (int r = 0; r < grid.length; r++) {
+			for (int c = 0; c < grid[0].length; c++) {
+				if (grid[r][c] != 0)
+					return false;
+			}
+		}
+		return true;
+	}
+	
+}
